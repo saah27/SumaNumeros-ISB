8ed5fa8421f70d7b188f9c9516c9005ee8e392b2
saah27 <sergioalejandroah@isb.edu.mx>
2024-08-28 07:41:46 -0600

    Se sube el archivo al repositorio

---
+++ a1.py
@@ -0,0 +1,4 @@
+print("Bienvenido a la calculadora de peso y altura")
+peso=input("Ingresa tu peso en kilogramos")
+altura=input("Ingresa tu altura en centimetros")
+print("Tu peso es de", peso "tu altura es de", altura ) 
---
+++ suma.py
@@ -0,0 +1,11 @@
+a=int(input("ingresa el primer numero:"))
+b=int(input("Ingresa el segundo numero:"))
+
+
+#Suma de los numeros
+
+suma=a+b
+
+#Mostrar los resultados
+
+print("La suma es ", suma)
